ce61802179e3df9b83903ae9d04b3aaf1ce30934
Bruno Staine <brunostaine@hotmail.com>
2022-09-14 13:02:25 -0300

    create user

---
+++ src/main/java/com/dev/jpaestudos/JpaestudosApplication.java
@@ -0,0 +1,13 @@
+package com.dev.jpaestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaestudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaestudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/jpaestudos/JpaestudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.jpaestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaestudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
